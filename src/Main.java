5f2268b9cbb9e8c1396694ba140d424fdf391def
N1kk13 <n1kk1.nikk@yandex.ru>
2024-12-23 00:28:03 +0300

    Добавлен код для вычисления суммы, разности, произведения и частного двух чисел

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+       System.out.println("Введите первое число и нажмите <Enter>: ");
+       int num1 = new Scanner(System.in).nextInt();
+       System.out.println("Введите второе число и нажмите <Enter>: ");
+       int num2 = new Scanner(System.in).nextInt();
+
+       int sum = num1 + num2;
+       int difference = num1 - num2;
+       int product = num1 * num2;
+       double quotient = (double) num1 / num2;
+
+       System.out.println("Сумма: " + sum);
+       System.out.println("Разность: " + difference);
+       System.out.println("Произведение: " + product);
+       System.out.println("Частное: " + quotient);
     }
 }
