e63535abac1a8c93be31ce913e1e6da41dfbba2e
N1kk13 <n1kk1.nikk@yandex.ru>
2025-03-30 21:48:03 +0300

    Added a task on the topic of cycles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        Scanner scanner = new Scanner(System.in);
+        int fileCount = 0;
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+            File file = new File(path);
+
+            if (file.exists() && file.isFile()) {
+                System.out.println("Путь указан верно");
+                fileCount++;
+                System.out.printf("Это файл номер %d%n", fileCount);
+            } else {
+                if (!file.exists()) {
+                    System.out.println("Файл не существует.");
+                } else if (file.isDirectory()) {
+                    System.out.println("Указанный путь ведёт к папке, а не к файлу.");
+                } else {
+                    System.out.println("Указанный путь не является файлом.");
+                }
+            }
+        }
     }
 }
